be8f974624c7b7e1433f7f03aa2962839ad93a9a
unknown <manuxd29@gmail.com>
2020-11-18 21:00:52 +0100

    archivo php

---
+++ index.php
@@ -0,0 +1,17 @@
+
+ <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejercicio 2</title>
+    </head>
+   <body>
+       <?php
+
+       echo"EPD de git";
+
+
+ ?>
+</body>
+</html>
+
+
